e9b507c82bc37d83c77794835b29f7b84872a8a7
DESKTOP-U41BDTV\Drock <523166477@qq.com>
2019-07-05 18:32:18 +0800

    .

---
+++ core/anim_PM_MainWin.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# anim_PM_MainWin.py
+# @Author :  ()
+# @Link   : 
+# @Date   : 7/5/2019, 11:05:30 AM
+
+
+import sys, os, re
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from ctypes.wintypes import LONG, HWND, UINT, WPARAM, LPARAM, FILETIME
+import listItem_Proj
+import shutil # 文件夹操作
+
+
+# ------------------------ 主窗口 class -----------------------------#
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, uiPath='', parent=None):
+        super(MainWindow, self).__init__(parent)
+        # PyQt5 加载ui文件方法
+        self.ui = uic.loadUi(uiPath, self)
+
+        # ------------- 概览 ------------- #
+        item = QtWidgets.QListWidgetItem() # 创建QListWidgetItem对象
+        item.setSizeHint(QtCore.QSize(200, 50))  # 设置QListWidgetItem大小
+        itemWidget = listItem_Proj.ListItem_Proj() # itemWidget
+        self.ui.listWidget__General_Proj.addItem(item) # 添加item
+        self.ui.listWidget__General_Proj.setItemWidget(item, itemWidget)  # 为item设置widget
+
+        
+    def closeEvent(self, event):
+        '''
+        重写closeEvent方法
+        '''
+        event.accept()
+        quit()
+
+
+def main():
+    # print(os.path.isdir(amConfigure.getProjectPath()))
+    # 启动窗口
+    global w
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow(os.path.dirname(os.path.dirname(__file__)) +\
+        '/UI/PM_Anim_MainWin.ui')
+    w.show()
+                   
+    sys.exit(app.exec_()) 
+
+
+if __name__ == '__main__':
+    main()
--- core/listItem_Proj.py
+++ core/listItem_Proj.py
@@ -5,3 +5,17 @@
 # @Author :  ()
 # @Link   : 
 # @Date   : 7/2/2019, 5:16:22 PM
+
+
+import sys
+from PyQt5 import QtWidgets, uic
+
+
+# 项目列表项
+class ListItem_Proj(QtWidgets.QWidget):
+    def __init__(self, uiPath='', parent=None):
+        super(ListItem_Proj, self).__init__(parent)
+
+        uiPath = './UI/listItem_Proj.ui'
+        # PyQt5 加载ui文件方法
+        self.ui = uic.loadUi(uiPath, self)
