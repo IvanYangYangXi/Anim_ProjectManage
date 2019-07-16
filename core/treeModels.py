ee2ad99ead64f0fc0cae16417050fa6785a7dd7d
IvanYang <523166477@qq.com>
2019-07-16 22:02:56 +0800

    /

--- core/treeModels.py
+++ core/treeModels.py
@@ -21,3 +21,248 @@ class TreeItemBase(object):
 
         if parent:
             parent.appendChild(self)
+
+    def typeInfo(self):
+        return "None"
+
+    def appendChild(self, item):
+        self._childItems.append(item)
+
+    def insertChild(self, position, child):
+        if position < 0 or position > len(self._childItems):
+            return False
+        
+        self._childItems.insert(position, child)
+        child._parentItem = self
+        return True
+
+    def removeChild(self, position):
+        if position < 0 or position > len(self._childItems):
+            return False
+
+        child = self._childItems.pop(position)
+        child._parentItem = None
+
+    def child(self, row):
+        return self._childItems[row]
+
+    def childCount(self):
+        return len(self._childItems)
+
+    def columnCount(self):
+        return len(self._itemData)
+
+    def data(self):
+        return self._itemData
+
+    def setData(self, value):
+        self._itemData = value
+
+    def parent(self):
+        return self._parentItem
+
+    def row(self):
+        if self._parentItem:
+            return self._parentItem._childItems.index(self)
+
+        return 0
+
+    def log(self, tablevel=-1):
+        output = '--'
+        tablevel += 1
+        
+        for i in range(tablevel):
+            output += "\t"
+
+        output =output + "|------" + self._itemData + "\n"
+
+        for child in self._childItems:
+            output =output + child.log(tablevel)
+
+        tablevel -= 1
+        return output
+
+    def __repr__(self): # 返回一个可以用来表示对象的可打印字符串
+        return self.log()
+
+
+class TreeModel(QtCore.QAbstractItemModel):
+    def __init__(self, data, parent=None):
+        super(TreeModel, self).__init__(parent)
+
+        # 设置初始项的内容
+        self._rootItem = data
+        
+
+    # 设置列数
+    def columnCount(self, parent):
+        return 3
+
+    # 设置行数
+    def rowCount(self, parent):
+        # 当父项有效时，rowCount（）应返回0。
+        if parent.column() > 0:
+            return 0
+        
+        if not parent.isValid():
+            parentItem = self._rootItem
+        else:
+            parentItem = parent.internalPointer()
+
+        return parentItem.childCount()
+
+    # 返回索引项目存储的数据。
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+
+        # item = index.internalPointer()
+        item = self.getItem(index)
+
+        if role == QtCore.Qt.DisplayRole or role == QtCore.Qt.EditRole:
+            if index.column() == 0:
+                return item.data()
+            elif index.column() == 2: 
+                return item.typeInfo()
+
+        if role == QtCore.Qt.DecorationRole:
+            if index.column() == 0:
+                typeInfo = item.typeInfo()
+
+                if typeInfo == "Camera":
+                    return QtGui.QIcon(QtGui.QPixmap('./UI/qt-logo.png'))
+
+    # 返回一组标志
+    def flags(self, index):
+
+        # 复选框
+        if index.column() == 2:
+            return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsUserCheckable
+
+        # 基类实现返回一组标志，标志启用item（ItemIsEnabled），并允许它被选中（ItemIsSelectable）。
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEditable
+
+    # 编辑数据
+    def setData(self, index, value, role=QtCore.Qt.EditRole):
+        if index.isValid():
+            item = index.internalPointer()
+
+            if role == QtCore.Qt.EditRole:
+                if index.column() == 0:
+                    item.setData(value)
+                    self.dataChanged.emit(index, index) # 更新Model的数据
+
+                return True
+
+        return False
+
+    # 返回给定索引项的父级。如果项目没有父项，则返回无效的QModelIndex。
+    def parent(self, index):
+        if not index.isValid():
+            return QtCore.QModelIndex()
+
+        item = index.internalPointer()
+        parentItem = item.parent()
+
+        if parentItem == self._rootItem:
+            return QtCore.QModelIndex()
+
+        return self.createIndex(parentItem.row(), 0, parentItem)
+
+    # 返回给定行、列和父索引指定的模型中，项的索引。
+    def index(self, row, column, parent):
+        # 索引不存在，返回无效索引
+        if not self.hasIndex(row, column, parent):
+            return QtCore.QModelIndex()
+        
+        # 如果父项不存在，设置 parentItem = rootItem
+        if not parent.isValid():
+            parentItem = self._rootItem
+        else:
+            parentItem = parent.internalPointer()
+
+        item = parentItem.child(row)
+        if item:
+            # 子类中重写此函数时，调用createIndex()以生成其他组件,用于项的模型索引。
+            return self.createIndex(row, column, item)
+        else:
+            return QtCore.QModelIndex()
+
+    # 设置标题行
+    def headerData(self, section, orientation, role):
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            if section == 0:
+                return "Title"
+            else:
+                return "typeInfo"
+
+        return None
+
+    # 自定义函数，获取索引项
+    def getItem(self, index):
+        if index.isValid():
+            item = index.internalPointer()
+            if item:
+                return item
+        
+        return self._rootItem
+  
+    # 插入多列数据
+    def insertRows(self, position, rows, parent = QtCore.QModelIndex()):
+        
+        parentItem = self.getItem(parent)
+        self.beginInsertRows(parent, position, position + rows - 1) # index, first, last
+                
+        for i in range(rows):
+            childItem = TreeItem('insert item %d'%i)
+            isSuccess = parentItem.insertChild(position, childItem)
+
+        self.endInsertRows()
+
+        return isSuccess
+    
+    # 删除多行数据（插入位置， 插入行数， 父项(默认父项为空项)）
+    def removeRows(self, position, rows, parent = QtCore.QModelIndex()):
+        
+        parentItem = self.getItem(parent)
+        self.beginRemoveRows(parent, position, position + rows - 1)
+
+        for i in range(rows):
+            isSuccess = parentItem.removeChild(position)
+
+        self.endRemoveRows()
+
+        return isSuccess
+
+
+# Item class
+class SpinBoxDelegate(QtWidgets.QItemDelegate):
+    '''
+    '''
+    # createEditor 返回用于更改模型数据的小部件，可以重新实现以自定义编辑行为。
+    def createEditor(self, parent, option, index):
+        editor = QtWidgets.QSpinBox(parent)
+        editor.setFrame(False)
+        editor.setFrame(False)
+        editor.setMinimum(0)
+        editor.setMaximum(100)
+
+        return editor
+
+    # 设置编辑器从模型索引指定的数据模型项中显示和编辑的数据。
+    def setEditorData(self, spinBox, index):
+        value = index.model().data(index, QtCore.Qt.EditRole)
+
+        spinBox.setValue(6)
+
+    # 从编辑器窗口小部件获取数据，并将其存储在项索引处的指定模型中。
+    def setModelData(self, spinBox, model, index):
+        spinBox.interpretText()
+        value = spinBox.value()
+
+        model.setData(index, value, QtCore.Qt.EditRole)
+
+    # 根据给定的样式选项更新索引指定的项目的编辑器。
+    def updateEditorGeometry(self, editor, option, index):
+        editor.setGeometry(option.rect)
+
